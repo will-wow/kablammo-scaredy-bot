0a050a15ed578b36f7a5012780011885c2811d21
Jason Wadsworth <jdwadsworth@gmail.com>
2013-09-19 21:43:55 -0700

    Added comments with notes about available commands

--- strategy.rb
+++ strategy.rb
@@ -1,5 +1,22 @@
-# f - fire!
+# Game Info
+# 
+# board -- the game board
+#   See: https://github.com/carbonfive/kablammo-strategy/blob/master/lib/strategy/models/board.rb
+# robot -- you
+#   See: https://github.com/carbonfive/kablammo-strategy/blob/master/lib/strategy/models/robot.rb
+# opponents -- opponent robots
+#   See: https://github.com/carbonfive/kablammo-strategy/blob/master/lib/strategy/models/robot.rb
+# 
+# Actions:
+# 
+# fire!(skew=0) -- shot at something
+# rotate!(degrees) -- rotate the turret
+# rest -- rest and reload
+# move_north! -- move north
+# move_south! -- move south
+# move_east! -- move east
+# move_west! -- move west
 
 on_turn do
-  'f'
+  fire!
 end
